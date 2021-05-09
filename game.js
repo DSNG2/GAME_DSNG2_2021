d5ce037589c74803da8722871e506165f71311e2
Karolaine <karolbt04@gmail.com>
2021-05-09 20:31:27 -0300

    commit Karol com o sprite

--- game.js
+++ game.js
@@ -174,3 +174,71 @@ window.onload = function(){
 		draw();
 	}
 }
+function Sprite(img){
+	//Atributos ******
+	this.mvLeft = this.mvUp = this.mvRight = this.mvDown = false;
+	
+	//Origem para captura da imagem a ser exibida
+	this.srcX = this.srcY = 0;
+	//Posição no canvas onde a figura será exibida
+	this.posX = this.posY = 0;
+	this.width = 24;
+	this.height = 32;
+	this.speed = 1;
+	this.img = img;
+	this.countAnim = 0;
+
+	//Métodos *******
+	//Desenha a figura
+	this.draw = function(ctx){
+		ctx.drawImage(	this.img,	//Imagem de origem
+						//Captura da imagem
+						this.srcX,	//Origem da captura no eixo X
+						this.srcY,	//Origem da captura no eixo Y
+						this.width,	//Largura da imagem que será capturada
+						this.height,//Altura da imagem que será capturada
+						//Exibição da imagem
+						this.posX,	//Posição no eixo X onde a imagem será exibida 
+						this.posY,	//Posição no eixo Y onde a imagem será exibida 
+						this.width,	//Largura da imagem a ser exibida 
+						this.height	//Altura da imagem a ser exibida 
+					);
+		this.animation();
+	}
+
+	//Move a figura
+	this.move = function(){
+		if(this.mvRight){
+			this.posX += this.speed;
+			this.srcY = this.height * 3; 
+		} else
+		if(this.mvLeft){
+			this.posX -= this.speed;
+			this.srcY = this.height * 2; 
+		} else
+		if(this.mvUp){
+			this.posY -= this.speed;
+			this.srcY = this.height * 1; 
+		} else
+		if(this.mvDown){
+			this.posY += this.speed;
+			this.srcY = this.height * 0; 
+		}
+	}
+	
+	//Anima a figura
+	this.animation = function(){
+		if(this.mvLeft || this.mvUp || this.mvRight || this.mvDown){
+			//Caso qualquer seta seja pressionada, o contador de animação é incrementado
+			this.countAnim++;
+			if(this.countAnim >= 40){
+				this.countAnim = 0;
+			}
+			this.srcX = Math.floor(this.countAnim / 5) * this.width;
+		} else {
+			//Caso nenhuma tecla seja pressionada, o contador de animação é zerado e a imagem do personagem parado é exibida
+			this.srcX = 0;
+			this.countAnim = 0;
+		}
+	}
+}
